df646a8d36270ecd54bf80fa77c41f0c0d898b1a
Ruben <rubentenderini@gmail.com>
2026-04-23 14:36:44 +0200

    splunk instro to splunk obs cloud

--- src/splunk-instrumentation.js
+++ src/splunk-instrumentation.js
@@ -1,13 +1,14 @@
 import SplunkOtelWeb from '@splunk/otel-web';
 
 SplunkOtelWeb.init({
-    beaconEndpoint: import.meta.env.VITE_COLLECTOR_URL,
-    //realm: import.meta.env.VITE_RUM_REALM,
-    rumAccessToken: "",
+    //beaconEndpoint: import.meta.env.VITE_COLLECTOR_URL,
+    realm: import.meta.env.VITE_RUM_REALM,
+    rumAccessToken: import.meta.env.VITE_RUM_ACCESS_TOKEN,
     applicationName: 'atomic-crm',
     deploymentEnvironment: 'local-development',
     allowInsecureBeacon: true,
     exporter: {
         otlp: true
     },
+    spaMetrics: true,
 });
