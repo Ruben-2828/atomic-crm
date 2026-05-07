502fcaa371bd5dc9c89e47317164af6774859f03
Ruben <rubentenderini@gmail.com>
2026-05-07 16:06:50 +0200

    test zipkin

--- src/splunk-instrumentation.js
+++ src/splunk-instrumentation.js
@@ -3,12 +3,12 @@ import SplunkOtelWeb from '@splunk/otel-web';
 SplunkOtelWeb.init({
     beaconEndpoint: import.meta.env.VITE_COLLECTOR_URL,
     //realm: import.meta.env.VITE_RUM_REALM,
-    //rumAccessToken: import.meta.env.VITE_RUM_ACCESS_TOKEN,
+    rumAccessToken: import.meta.env.VITE_RUM_ACCESS_TOKEN,
     applicationName: 'atomic-crm',
     deploymentEnvironment: 'aws-development',
     allowInsecureBeacon: true,
-    exporter: {
-        otlp: true
-    },
+    //exporter: {
+    //    otlp: true
+    //},
     spaMetrics: true,
 });
