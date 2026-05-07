bb61ae596bca9b3217e3b45b698daa65f7319aeb
Ruben <rubentenderini@gmail.com>
2026-05-07 10:59:52 +0200

    set endpoint to collector

--- src/splunk-instrumentation.js
+++ src/splunk-instrumentation.js
@@ -1,11 +1,11 @@
 import SplunkOtelWeb from '@splunk/otel-web';
 
 SplunkOtelWeb.init({
-    //beaconEndpoint: import.meta.env.VITE_COLLECTOR_URL,
-    realm: import.meta.env.VITE_RUM_REALM,
-    rumAccessToken: import.meta.env.VITE_RUM_ACCESS_TOKEN,
+    beaconEndpoint: import.meta.env.VITE_COLLECTOR_URL,
+    //realm: import.meta.env.VITE_RUM_REALM,
+    //rumAccessToken: import.meta.env.VITE_RUM_ACCESS_TOKEN,
     applicationName: 'atomic-crm',
-    deploymentEnvironment: 'local-development',
+    deploymentEnvironment: 'aws-development',
     allowInsecureBeacon: true,
     exporter: {
         otlp: true
